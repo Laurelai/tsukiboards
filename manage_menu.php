9527da6a510372eaa5ab56cb930863a1cbff9fac
Laurelai <Laurelai@oneechan.org>
2011-04-27 22:38:59 -0700

    Added menu mods

--- manage_menu.php
+++ manage_menu.php
@@ -1,19 +1,11 @@
 <?php
 /*
- * This file is part of kusaba.
+ * ONEECHAN 1.0.2
  *
- * kusaba is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
+ * ONEECHAN IS BASED ON KUSABA X 0.9.2
  *
- * kusaba is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * http://img.oneechan.org/
  *
- * You should have received a copy of the GNU General Public License along with
- * kusaba; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 /**
  * Manage menu
@@ -56,12 +48,13 @@ if (!$manage_class->ValidateSession(true)) {
 		$tpl_links .= _gettext('Janitor');
 	}
 	$tpl_links .= "</strong>";
-	$tpl_links .= '<li><a href="' . KU_WEBFOLDER . '" target="_top">' . _gettext('Home') . '</a></li>' . "\n";
+	$tpl_links .= '<li><a href="' . KU_WEBFOLDER . '" target="_top">' . _gettext('Oneechan') . '</a></li>' . "\n";
 	$tpl_links .= '<li><a href="manage_page.php?action=logout">'._gettext('Log out').'</a></li>
 	<li><span id="postingpassword"><a id="showpwd" href="#" onclick="javascript:document.getElementById(\'postingpassword\').innerHTML = \'<input type=text id=postingpasswordbox value=' . $manage_postpassword . '>\'; document.getElementById(\'postingpasswordbox\').select(); return false;">'._gettext('Show Posting Password').'</a></span></li></ul>';
 	// Home
 	$tpl_links .= section_html(_gettext('Home'), 'home') .
 	'<ul>
+	<li><a href="http://mail.oneechan.org/" target="_blank">' . _gettext('Staff Email System') . '</a></li>
 	<li><a href="manage_page.php?">'._gettext('View Announcements').'</a></li>
 	<li><a href="manage_page.php?action=posting_rates">'._gettext('Posting rates (past hour)').'</a></li>
 	<li><a href="manage_page.php?action=statistics">' . _gettext('Statistics') . '</a></li>';
@@ -71,37 +64,49 @@ if (!$manage_class->ValidateSession(true)) {
 	$tpl_links .= '</ul></div>';
 	// Administration
 	if ($manage_class->CurrentUserIsAdministrator()) {
-		$tpl_links .= section_html(_gettext('Site Administration'), 'siteadministration') .
-		'<ul>' . "\n" .
-		'<li><a href="manage_page.php?action=addannouncement">' . _gettext('Announcements') . '</a></li>' . "\n" .
-		'<li><a href="manage_page.php?action=news">' . _gettext('News') . '</a></li>' . "\n" .
-		'<li><a href="manage_page.php?action=faq">' . _gettext('FAQ') . '</a></li>' . "\n" .
-		'<li><a href="manage_page.php?action=rules">' . _gettext('Rules') . '</a></li>' . "\n";
-		if (KU_BLOTTER) $tpl_links .= '<li><a href="manage_page.php?action=blotter">' . _gettext('Blotter') . '</a></li>';
-		$tpl_links .= '<li><a href="manage_page.php?action=templates">' . _gettext('Edit templates') . '</a></li>
+		$tpl_links .= section_html(_gettext('Templates &amp; Appearance'), 'siteadministration', false) .
+		'<ul>
+		<li><a href="manage_page.php?action=index">' . _gettext('Index Page Management') . '</a></li>
+		<li><a href="manage_page.php?action=faq">' . _gettext('FAQ Page Management') . '</a></li>
+		<li><a href="manage_page.php?action=news">' . _gettext('News') . '</a></li>' . "\n" . '</a></li>
+		<li><a href="manage_page.php?action=rules">' . _gettext('Rules Page Management') . '</a></li>
+		<li><a href="manage_page.php?action=staffpage">' . _gettext('Staff Page Management') . '</a></li>
+		<li><a href="manage_page.php?action=legal">' . _gettext('Legal Page Management') . '</a></li>
+		<li><a href="manage_page.php?action=boardman">' . _gettext('Edit Board Table') . '</a></li>
+		<li><a href="manage_page.php?action=addannouncement">' . _gettext('Edit Announcements') . '</a></li>
+		<li><a href="manage_page.php?action=blotter">' . _gettext('Blotter') . '</a></li>
+		<li><a href="manage_page.php?action=templates">' . _gettext('Edit templates') . '</a></li>
+		<li><a href="manage_page.php?action=tempheader">' . _gettext('Edit Main Header') . '</a></li>
+		<li><a href="manage_page.php?action=tempfooter">' . _gettext('Edit Main Footer') . '</a></li>
+		<li><a href="manage_page.php?action=ads">' . _gettext('Manage Ads') . '</a></li>
+		<li><a href="manage_page.php?action=editsections">' . _gettext('Edit sections') . '</a></li>
+		</ul></div>';
+		$tpl_links .= section_html(_gettext('System Administration'), 'chanadministration') .
+		'<ul>
 		<li><a href="manage_page.php?action=spaceused">' . _gettext('Disk space used') . '</a></li>
 		<!--<li><a href="manage_page.php?action=checkversion">' . _gettext('Check for new version') . '</a></li>-->
 		<li><a href="manage_page.php?action=staff">' . _gettext('Staff') . '</a></li>
 		<li><a href="manage_page.php?action=modlog">' . _gettext('ModLog') . '</a></li>
-		<li><a href="manage_page.php?action=proxyban">' . _gettext('Ban proxy list') . '</a></li>
 		<li><a href="manage_page.php?action=sql">' . _gettext('SQL query') . '</a></li>
-		<li><a href="manage_page.php?action=cleanup">' . _gettext('Cleanup') . '</a></li>' . "\n";
-		if (KU_APC) $tpl_links .= '<li><a href="manage_page.php?action=apc">APC</a></li>' . "\n";
-		$tpl_links .= '</ul></div>' .
-		section_html(_gettext('Boards Administration'), 'boardsadministration') .
+		<li><a href="manage_page.php?action=cleanup">' . _gettext('Cleanup') . '</a></li>
+		<li><a href="manage_page.php?action=rebuildall">' . _gettext('Rebuild HTML') . '</a></li>' . "\n";
+		if (KU_APC) $tpl_links .= '<li><a href="manage_page.php?action=apc">APC</a></li>
+		</ul></div>';
+		$tpl_links .= section_html(_gettext('Content Administration'), 'contadministration') .
 		'<ul>
-		<li><a href="manage_page.php?action=adddelboard">' . _gettext('Add/Delete boards') . '</a></li>
 		<li><a href="manage_page.php?action=wordfilter">' . _gettext('Wordfilter') . '</a></li>
-		<li><a href="manage_page.php?action=spam">' . _gettext('Spamfilter') . '</a></li>
-		<li><a href="manage_page.php?action=ads">' . _gettext('Manage Ads') . '</a></li>
-		<li><a href="manage_page.php?action=embeds">' . _gettext('Manage embeds') . '</a></li>
-		<li><a href="manage_page.php?action=movethread">' . _gettext('Move thread') . '</a></li>
+		<li><a href="manage_page.php?action=spam">' . _gettext('Block &amp; Ban Content') . '</a></li>
+		<li><a href="manage_page.php?action=blockfilter">' . _gettext('Block Content') . '</a></li>
 		<li><a href="manage_page.php?action=ipsearch">' . _gettext('IP Search') . '</a></li>
 		<li><a href="manage_page.php?action=search">' . _gettext('Search posts') . '</a></li>
 		<li><a href="manage_page.php?action=viewthread">'._gettext('View thread (including deleted)').'</a></li>
-		<li><a href="manage_page.php?action=editfiletypes">' . _gettext('Edit filetypes') . '</a></li>
-		<li><a href="manage_page.php?action=editsections">' . _gettext('Edit sections') . '</a></li>
-		<li><a href="manage_page.php?action=rebuildall">' . _gettext('Rebuild all html files') . '</a></li>' . "\n" .
+		</ul></div>'  . "\n";
+		$tpl_links .= section_html(_gettext('Board Management'), 'boardsn') .
+		'<ul>
+		<li><a href="manage_page.php?action=boardopts">' . _gettext('Board options') . '</a></li>
+		<li><a href="manage_page.php?action=adddelboard">' . _gettext('Add/Delete boards') . '</a></li>
+		<li><a href="manage_page.php?action=embeds">' . _gettext('Manage embeds') . '</a></li>
+		<li><a href="manage_page.php?action=editfiletypes">' . _gettext('Edit filetypes') . '</a></li>' . "\n" .
 		'</ul></div>';
 
 		/*$tpl_links .= section_html(_gettext('Modules'), 'modules') .
@@ -113,26 +118,23 @@ if (!$manage_class->ValidateSession(true)) {
 		$tpl_links .= '</ul></div>';*/
 	}
 	// Boards
-	$tpl_links .= section_html(_gettext('Boards'), 'boards') .
+	$open_reports = $tc_db->GetAll("SELECT HIGH_PRIORITY COUNT(*) FROM `" . KU_DBPREFIX . "reports` WHERE `cleared` = '0'");
+	$tpl_links .= section_html(_gettext('Moderation'), 'mods') .
 	'<ul>
-	<li><a href="manage_page.php?action=boardopts">' . _gettext('Board options') . '</a></li>
 	<li><a href="manage_page.php?action=stickypost">' . _gettext('Manage stickies') . '</a></li>
 	<li><a href="manage_page.php?action=lockpost">' . _gettext('Manage locked threads') . '</a></li>
 	<li><a href="manage_page.php?action=delposts">' . _gettext('Delete thread/post') . '</a></li>
+	<li><a href="manage_page.php?action=movethread">' . _gettext('Move thread') . '</a></li>
+	<li><a href="manage_page.php?action=reports">' . _gettext('View Reports') . ' [' . $open_reports[0][0] . ']</a></li>
+	<li><a href="manage_page.php?action=proxyban">' . _gettext('Ban proxy list') . '</a></li>
+	<li><a href="manage_page.php?action=bans">' . _gettext('View/Add/Remove bans') . '</a></li>
+	<li><a href="manage_page.php?action=chanspambl">' . _gettext('Chan Spam Blacklist') . '</a></li>
+	<li><a href="manage_page.php?action=appeals">' . _gettext('View Appeals') . '</a></li>
+	<li><a href="manage_page.php?action=deletepostsbyip">' . _gettext('Delete all posts by IP') . '</a></li>
+	<li><a href="manage_page.php?action=recentimages">' . _gettext('Recently uploaded images') . '</a></li>
+	<li><a href="manage_page.php?action=recentposts">' . _gettext('Recent posts') . '</a></li>
 	</ul></div>';
-	// Moderation
-	if ($manage_class->CurrentUserIsAdministrator() || $manage_class->CurrentUserIsModerator()) {
-		$open_reports = $tc_db->GetAll("SELECT HIGH_PRIORITY COUNT(*) FROM `" . KU_DBPREFIX . "reports` WHERE `cleared` = '0'");
-		$tpl_links .= section_html(_gettext('Moderation'), 'moderation') .
-		'<ul>
-		<li><a href="manage_page.php?action=reports">' . _gettext('View Reports') . ' [' . $open_reports[0][0] . ']</a></li>
-		<li><a href="manage_page.php?action=bans">' . _gettext('View/Add/Remove bans') . '</a></li>';
-		if (KU_APPEAL) $tpl_links .= '<li><a href="manage_page.php?action=appeals">' . _gettext('View Appeals') . '</a></li>';
-		$tpl_links .= '<li><a href="manage_page.php?action=deletepostsbyip">' . _gettext('Delete all posts by IP') . '</a></li>
-		<li><a href="manage_page.php?action=recentimages">' . _gettext('Recently uploaded images') . '</a></li>
-		<li><a href="manage_page.php?action=recentposts">' . _gettext('Recent posts') . '</a></li>
-		</ul></div>';
-	}
+
 
 	if ($manage_class->CurrentUserIsModerator() ) {
 		$tpl_links .= section_html(_gettext('Moderating Boards'), 'mboards', false) . '<ul>';
