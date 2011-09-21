93ad05cb99e005b8b5d6bda3402869e346ac51ad
Bob Hayes <nostromo@nym.hush.com>
2011-09-21 22:54:54 +0100

    Doh ... faptcha.php fix to allow for mixed-case filenames

--- faptcha.php
+++ faptcha.php
@@ -22,20 +22,20 @@ while (false !== ($filename = readdir($dh)))
 }	
 closedir($dh);
 
-$NumFiles = count($files) - 1;
+$NumFiles = count($files);
 if( $NumFiles <= 0 )
 {
-	die("No faptcha images found! Please place them in faptchas/");
+	die("No faptcha images found! Please place them in faptchas/");  // at least 2 are needed for it to work, actually ...
 }
 
 srand((double)microtime()*1000000);
-$randnum = rand(0,$NumFiles);
+$randnum = rand(0,$NumFiles - 1);
 $file = $dir . $files[$randnum];	
 $filename = $files[$randnum];
 
 
 // Tokenise filename into an array of acceptable answer words, set it as a session variable to check against user input later
-$filename2 = $filename;
+$filename2 = strtolower($filename);
 $trailingNumPattern = '/\s_*[0-9]+\.(jpg|jpeg|png)$/';			// trailing numbers: zero or more of any char (e.g. _) followed by one or more integers followed by the extension / EOL
 $filename2 = preg_replace( $trailingNumPattern, '', $filename2 );	// replace with nothing, i.e. ignore them
 $filename2 = preg_replace('/.(jpg|jpeg|png)$/', '', $filename2 );	// remove extensions not caught by the above (unnumbered files)
