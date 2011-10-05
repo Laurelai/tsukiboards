5b3efe250fe172e63f92e257d94b94445bb05aa6
Laurelai <Laurelai@oneechan.org>
2011-10-04 17:34:18 -0700

    arcNET rebranding

--- kusaba.php
+++ kusaba.php
@@ -1,4 +1,27 @@
 <?php
+/*
+ * This file is part of arcNET
+ *
+ * arcNET uses core code from Kusaba X and Oneechan
+ *
+ * tsukihi.me kusabax.cultnet.net oneechan.org
+ *
+ * arcNET is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * kusaba is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * kusaba; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * credits to jmyeom for improving this
+ *
+ */
 if (file_exists("install.php")) {
 	die('You are seeing this message because either you haven\'t ran the install file yet, and can do so <a href="install.php">here</a>, or already have, and <strong>must delete it</strong>.');
 }
