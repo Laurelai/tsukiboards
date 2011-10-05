40f081c3f3287a6c7b2e8fec5e11c0df60d5e54e
Laurelai <Laurelai@oneechan.org>
2011-10-04 17:36:25 -0700

    arcNET rebranding

--- news.php
+++ news.php
@@ -1,13 +1,26 @@
 <?php
 /*
- /*
- * arcNET 0.2
+ * This file is part of arcNET
  *
- * arcNET uses core code from ONEECHAN and KUSABA X 0.9.3
+ * arcNET uses core code from Kusaba X and Oneechan
  *
- * http://tsukihi.me
+ * tsukihi.me kusabax.cultnet.net oneechan.org
  *
- * http://img.oneechan.org
+ * arcNET is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * kusaba is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * kusaba; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * credits to jmyeom for improving this
+ *
  */
  
 /**
