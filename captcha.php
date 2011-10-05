5d9cfbc9c384e433d6525cae1d6f3dc53a724293
Laurelai <Laurelai@oneechan.org>
2011-10-04 17:29:25 -0700

    arcNET rebranding

--- captcha.php
+++ captcha.php
@@ -1,4 +1,27 @@
 <?php
+/*
+ * This file is part of arcNET
+ *
+ * arcNET uses core code from Kusaba X and Oneechan
+ *
+ * tsukihi.me kusabax.cultnet.net oneechan.org
+ *
+ * arcNET is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * kusaba is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * kusaba; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * credits to jmyeom for improving this
+ *
+ */
 /**
  * RH - Cheesy "redirector" of requests to the old captcha.php to faptcha.php
  * This should fix dollchan extension tools without user having to mod the script.
