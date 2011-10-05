b5b12a6467203140f2cdcea32da96c14ec2c558a
Laurelai <Laurelai@oneechan.org>
2011-10-04 17:33:18 -0700

    arcNET rebranding

--- expand.php
+++ expand.php
@@ -1,8 +1,12 @@
 <?php
 /*
- * This file is part of kusaba.
+ * This file is part of arcNET
  *
- * kusaba is free software; you can redistribute it and/or modify it under the
+ * arcNET uses core code from Kusaba X and Oneechan
+ *
+ * tsukihi.me kusabax.cultnet.net oneechan.org
+ *
+ * arcNET is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
@@ -14,6 +18,9 @@
  * You should have received a copy of the GNU General Public License along with
  * kusaba; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * credits to jmyeom for improving this
+ *
  */
 /**
  * AJAX thread expansion handler
