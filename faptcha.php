497802417a55572625ba33b773d4fcd3ae623263
Bob Hayes <nostromo@nym.hush.com>
2011-11-19 07:46:10 +0000

    Faptcha is now somewhat stronger, uses ImageMagick

--- faptcha.php
+++ faptcha.php
@@ -1,30 +1,31 @@
 <?php
 /*
- * This file is part of arcNET
- *
- * arcNET uses core code from Kusaba X and Oneechan
- *
- * tsukihi.me kusabax.cultnet.net oneechan.org
- *
- * arcNET is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * kusaba is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * kusaba; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
- *
- * credits to jmyeom for improving this
- *
- */
+* This file is part of arcNET
+*
+* arcNET uses core code from Kusaba X and Oneechan
+*
+* tsukihi.me kusabax.cultnet.net oneechan.org
+*
+* arcNET is free software; you can redistribute it and/or modify it under the
+* terms of the GNU General Public License as published by the Free Software
+* Foundation; either version 2 of the License, or (at your option) any later
+* version.
+*
+* kusaba is distributed in the hope that it will be useful, but WITHOUT ANY
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+* A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* kusaba; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*
+* credits to jmyeom for improving this
+*
+*/
 /** 
  * Animu Character Image Captcha, Oneechan edition
  * Inspired by the "Flower Bus Engine" of 410chan.ru, and like theirs adapted from the (crappy) "faptcha" implementation in Serissa
+ * Now uses ImageMagick
  * 
  * @package kusaba  
  */
@@ -69,77 +70,77 @@ $_SESSION['faptcha_answers'] = $words;	// assign them to session variable
 // Serve the image
 if( ".png" == substr( $file, -4) )
 {
-	$image = imagecreatefrompng($file);
-	if( FALSE == $image )
-	{
-		error_log('Faptcha serve failed, ' . $file . ' is not a valid PNG!');	// this has happened ... identify such cases via error log
-	}
-	$image = ImageMangle( $image );
+	$image = ImageMangle( $file );
 	header('Content-Type: image/png');	
-	imagepng($image);
+	echo $image;
 }
 else if( ".jpg" == substr( $file, -4) || ".jpeg" == substr( $file, -5) )
 {
-	$image = imagecreatefromjpeg($file);
-	if( FALSE == $image )
-	{
-		error_log('Faptcha serve failed, ' . $file . ' is not a valid JPEG!');
-	}
-	$image = ImageMangle( $image );
+	$image = ImageMangle( $file );
 	header('Content-Type: image/jpeg');	
-	imagejpeg($image);
+	echo $image;
 }
 
-function ImageMangle( $image )
+function ImageMangle( $file )
 {
-	// Image mangling, to prevent a trivial hash matching database attack.
-	// For now we just draw a couple of random coloured lines on it (an improved version of what 410chan do.)
-	// TODO: Not as strong as I'd like. Perspective deformation etcetera via ImageMagick?
-	$width = imagesx($image);
-	$height = imagesy($image);
-	$noise_color = imagecolorallocate($image, rand(0,255), rand(0,255), rand(0,255));
-	imageline($image, mt_rand(0,$width), mt_rand(0,$height), mt_rand(0,$width), mt_rand(0,$height), $noise_color);
-	$noise_color = imagecolorallocate($image, rand(0,255), rand(0,255), rand(0,255));
-	imageline($image, mt_rand(0,$width), mt_rand(0,$height), mt_rand(0,$width), mt_rand(0,$height), $noise_color);
+	// Image mangling. This prevents trivial database hash matching, and is supposed to also defend against image recognition services.
+	// Unfortunately it turns out that some of these are quite good ... the below is enough to defeat them most of the time.
+	// TODO: Some sort of mild warping? Perspective deformation kind of works ...
+	$image = new Imagick($file);
+	$width = $image->getImageWidth();
+	$height = $image->getImageHeight();
+	
+	// Randomly rotate 8 - 40 degrees one way or the other, random background colour.
+	$bg = new ImagickPixel();
+	$bg->setColor( GetRandomColour() );
+	$rotate = 0;
+	$rotate = mt_rand(-40,40);
+	if( $rotate >=0 && $rotate < 7 )
+		$rotate += 8;
+	else if( $rotate <= 0 && $rotate >-7 )
+		$rotate -= 8;
+	$image->rotateImage( $bg, $rotate ); 
 
-	// Randomly resize it a bit. Mostly an additional defence against a filesize matching attack.
-	// 5px seems to be enough to introduce useful variation without messing up the postbox layout or making it too small.
-	$resizePix = 0;
-	while( 0 == $resizePix )
-		$resizePix = rand(-5,5);
-	$image = ResizeImage( $image, $width, $height, $width+$resizePix, $height+$resizePix, FALSE );
+	// Draw 2 random lines as before, thickness also randomised a bit now
+	$draw = new ImagickDraw();
+	$draw->setStrokeColor( GetRandomColour() );
+	$draw->setStrokeWidth( mt_rand(1,3) );
+	$draw->line( mt_rand(0, $width), mt_rand(0,$height), mt_rand(0,$width), mt_rand(0,$height) );
+	$draw->setStrokeColor( GetRandomColour() );
+	$draw->setStrokeWidth( mt_rand(1,3) );
+	$draw->line( mt_rand(0, $width), mt_rand(0,$height), mt_rand(0,$width), mt_rand(0,$height) );
+	$image->drawImage( $draw );
+
+	// Crop it a bit
+	$image->cropImage( $image->getImageWidth() - 10, $image->getImageHeight() - 10, 5, 5 );
+
+	// Shrink if neccessary
+	while( $image->getImageWidth() > 110 )
+	{
+		$newWidth = $image->getImageWidth() * 0.9;
+		$newHeight = $image->getImageHeight() * 0.9;
+		// High quality resize, bigger pics go blurry if we use scaleImage
+		$image->resizeImage( $newWidth, $newHeight, Imagick::FILTER_LANCZOS, 1 );
+	}
+
+	// Horizontal flip
+	$image->flopImage();
 
 	return $image;
 }
 
-
-function ResizeImage( $image, $width, $height, $w, $h, $crop=FALSE ) 
+function GetRandomColour()
 {
-    $r = $width / $height;
-    if ($crop) {
-        if ($width > $height) {
-            $width = ceil($width-($width*($r-$w/$h)));
-        } else {
-            $height = ceil($height-($height*($r-$w/$h)));
-        }
-        $newwidth = $w;
-        $newheight = $h;
-    } else {
-        if ($w/$h > $r) {
-            $newwidth = $h*$r;
-            $newheight = $h;
-        } else {
-            $newheight = $w/$r;
-            $newwidth = $w;
-        }
-    }
-    $src = $image;
-    $dst = imagecreatetruecolor($newwidth, $newheight);
-    imagecopyresampled($dst, $src, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
-
-    return $dst;
-}
+	// Returns a random colour in HTML triplet form, e.g. "#cc00cc"
+	$c = "";
+	for ($i = 0; $i<6; $i++)
+	{
+		$c .=  dechex(rand(0,15));
+	}
+	return "#$c";
+} 
 
 ?>
+
 
 
