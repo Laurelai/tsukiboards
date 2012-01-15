707b5a6782a233670b1050b9330872275f5e1cbb
Bob Hayes <nostromo@nym.hush.com>
2012-01-15 18:33:24 +0000

    Partial fix for issue #29 - blank faptcha answer on refresh. "Partial" since it doesn't work with dollchan, will probably be necessary to edit refreshCapImg() in dollchan to fix this.

--- inc/classes/board-post.class.php
+++ inc/classes/board-post.class.php
@@ -706,8 +706,9 @@ class Board {
 				// RH - Textbox to type the faptcha answer into = "captcha", calling it this reuses existing validation JS
 				//      Likewise we call the image 'captchaimage' as before
 				// 06/01/2012 SRC now also appends random querystring in case of caching issues
-				$faptcha_image = '<a href="#" onclick="javascript:document.getElementById(\'captchaimage\').src = \''. KU_CGIPATH . '/faptcha.php?\' + Math.random();return false;"> <IMG ID="captchaimage" SRC="'. KU_WEBFOLDER .'faptcha.php?'.mt_rand().'" border="0" alt="Click for a new faptcha" title="Click for a new faptcha"> </A>';
-				$faptcha_input = '<BR><input type="text" name="captcha" title="Enter character\'s name (common nicknames accepted ... probably)" size="28" maxlength="75" accesskey="c" />';
+				// 15/01/2012 blank answer textbox on refresh to avoid accidental wrong answers
+				$faptcha_image = '<a href="#" onclick="javascript:document.getElementById(\'captchaimage\').src = \''. KU_CGIPATH . '/faptcha.php?\' + Math.random();javascript:document.getElementById(\'captcha\').value=\'\';return false;"> <IMG ID="captchaimage" SRC="'. KU_WEBFOLDER .'faptcha.php?'.mt_rand().'" border="0" alt="Click for a new faptcha" title="Click for a new faptcha"> </A>';
+				$faptcha_input = '<BR><input type="text" name="captcha" id="captcha" title="Enter character\'s name (common nicknames accepted ... probably)" size="28" maxlength="75" accesskey="c" />';
 				$this->dwoo_data->assign('faptcha_image', $faptcha_image);	// the faptcha image
 				$this->dwoo_data->assign('faptcha_input', $faptcha_input);	// faptcha answer input textbox
 			}
